68fdd6493787628f91d0abf9c6f455d75729a571
ShubhamUpad <01shubhamupadhyay@gmail.com>
2025-03-07 16:15:44 +0530

    Finished with Pub-Sub

--- src/main/java/org/Prac2/EmitLog.java
+++ src/main/java/org/Prac2/EmitLog.java
@@ -1,4 +1,33 @@
 package org.Prac2;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args){
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        try (Connection connection = connectionFactory.newConnection();
+            Channel channel = connection.createChannel()){
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+
+            String message = args.length < 1 ? "info: Hello World ! ":
+                    String.join(" ", args);
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "' ");
+        }
+        catch (IOException | TimeoutException e){
+            System.out.println("Exception occurred " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
 }
